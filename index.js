2d2ed9a1b63c214e96df72d0e0f5a83aca53f918
Vadik1000-7 <petushok.v@mail.ru>
2023-02-09 19:31:46 +0300

    fix: js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-module.exports = function reverse(n) {
-    return +reverseStr(Math.abs(n).toString());
-}
-function reverseStr(str) {
-    if (str === '') return '';
-    return reverseStr(str.substr(1)) + str[0];
+module.exports = function reverse (n) {
+    
+    return parseFloat(n.toString().split('').reverse().join(''));
 }
